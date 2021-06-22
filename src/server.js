92f3e9215f56d4fb64e27eca48ed2ad7ec7b94a6
ggafiled <gafewkik234@gmail.com>
2021-06-22 16:13:05 +0700

    fix bug

--- src/server.js
+++ src/server.js
@@ -8,22 +8,21 @@ const router = require("./routes");
 require("dotenv").config();
 moment.locale("th");
 
-app.use(
-  cors({
-    credentials: true,
-    origin: "https://let-me-check.herokuapp.com",
-  })
-);
+app.use(cors());
 app.use(
   express.urlencoded({
     extended: false,
   })
 );
 app.use(bodyParser.json());
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
   next();
 });
 
