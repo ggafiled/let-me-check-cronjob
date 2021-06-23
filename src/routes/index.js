2ba490a04e1c23b91fcc6abcee5e30568fae09f2
ggafiled <gafewkik234@gmail.com>
2021-06-23 09:48:32 +0700

    fix bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -46,7 +46,8 @@ router.get("/checkout", async (req, res) => {
 router.options("/beacon-event", cors());
 router.get("/beacon-event", async (req, res) => {
   var options = {
-    uri: "https://randomuser.me/api/?format=xml",
+    method: "POST",
+    uri: "https://example.com/",
     body: {
       generatedId: "nayJlzoX0rerxUT9TgLAU",
     },
