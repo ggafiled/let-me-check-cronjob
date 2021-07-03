6662de750e594fb5c8ce48b9bbae209cb75ff05d
ggafiled <gafewkik234@gmail.com>
2021-07-03 18:26:45 +0700

    test petitio on heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -45,7 +45,6 @@ router.get("/beacon-event", async(req, res) => {
         let usertoken = await petitio(`https://api-scanner.thaichana.com/usertoken`, "POST").body({
             generatedId: "MCobJG-ILytTzB9-aVJ4__",
         }).header({
-            mode: "cors",
             "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
             credentials: "include",
         });
