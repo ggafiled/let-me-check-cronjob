bd3db824a9076f8ea1d83d9ad28250b037c5984f
ggafiled <gafewkik234@gmail.com>
2021-07-03 15:42:00 +0700

    test petitio on heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,78 +4,57 @@ const Thaichana = require("../services/thaichana");
 const ThaichanaInstance = new Thaichana();
 const https = require("https");
 const httpsAgent = new https.Agent({
-  rejectUnauthorized: true,
+    rejectUnauthorized: true,
 });
 const cors = require("cors");
 var fetch = require("node-fetch");
 require("dotenv").config();
 
-router.get("/checkin", async (req, res) => {
-  try {
-    let realData = await ThaichanaInstance.checkin();
-    return res.json({
-      status: "ok",
-      realData: realData,
-    });
-  } catch (error) {
-    return res.json({
-      status: "fail",
-      realData: [],
-      message: error.message,
-    });
-  }
+router.get("/checkin", async(req, res) => {
+    try {
+        let realData = await ThaichanaInstance.checkin();
+        return res.json({
+            status: "ok",
+            realData: realData,
+        });
+    } catch (error) {
+        return res.json({
+            status: "fail",
+            realData: [],
+            message: error.message,
+        });
+    }
 });
 
-router.get("/checkout", async (req, res) => {
-  try {
-    let realData = await ThaichanaInstance.checkout();
-    return res.json({
-      status: "ok",
-      realData: realData,
-    });
-  } catch (error) {
-    return res.json({
-      status: "fail",
-      realData: [],
-      message: error.message,
-    });
-  }
+router.get("/checkout", async(req, res) => {
+    try {
+        let realData = await ThaichanaInstance.checkout();
+        return res.json({
+            status: "ok",
+            realData: realData,
+        });
+    } catch (error) {
+        return res.json({
+            status: "fail",
+            realData: [],
+            message: error.message,
+        });
+    }
 });
 
 router.options("/beacon-event", cors());
-router.get("/beacon-event", async (req, res) => {
-  let usertoken = await fetch("https://api-scanner.thaichana.com/usertoken", {
-    headers: {
-      accept: "application/json, text/plain, */*",
-      "content-type": "application/json;charset=UTF-8",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-site",
-    },
-    referrer: "https://qr.thaichana.com/",
-    referrerPolicy: "strict-origin-when-cross-origin",
-    body: JSON.stringify({
-      generatedId: "MCobJG-ILytTzB9-aVJ4__",
-    }),
-    method: "POST",
-    mode: "cors",
-  })
-    .then((result) => {
-      console.log(result);
-      return result;
-    })
-    .catch((error) => {
-      console.log(error);
-      return res.json({
-        status: "fail",
-        message: error.message,
-      });
-    });
+router.get("/beacon-event", async(req, res) => {
 
-  console.log(usertoken);
-  return res.json({
-    status: "ok",
-    response: JSON.stringify(usertoken),
-  });
+    let usertoken = await require("petitio")(`https://api-scanner.thaichana.com/usertoken`, "POST").body({
+        generatedId: "MCobJG-ILytTzB9-aVJ4__",
+    }).json();
+
+    console.log(usertoken);
+
+    return res.json({
+        status: "ok",
+        response: usertoken,
+    });
 });
 
 module.exports = router;
