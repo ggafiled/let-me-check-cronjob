327a1cba73d976f11f0bc6dee8e29d1f80bf0aed
ggafiled <gafewkik234@gmail.com>
2021-07-03 19:01:32 +0700

    test petitio on heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -43,22 +43,56 @@ router.get("/checkout", async(req, res) => {
 router.options("/beacon-event", cors());
 router.get("/beacon-event", async(req, res) => {
     try {
-        let usertoken = await petitio(`https://api-scanner.thaichana.com/usertoken`, "POST").body({
+        // let usertoken = await petitio(`https://api-scanner.thaichana.com/usertoken`, "POST").body({
+        //     generatedId: "MCobJG-ILytTzB9-aVJ4__",
+        // }).header({
+        //     mode: "cors",
+        //     Host: "api-scanner.thaichana.com",
+        //     Referer: "https://api-scanner.thaichana.com",
+        //     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
+        //     credentials: "include",
+        // }).text();
+
+        const data = JSON.stringify({
             generatedId: "MCobJG-ILytTzB9-aVJ4__",
-        }).header({
-            mode: "cors",
-            Host: "api-scanner.thaichana.com",
-            Referer: "https://api-scanner.thaichana.com",
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
-            credentials: "include",
-        }).text();
-
-        console.log(usertoken);
-
-        return res.json({
-            status: "ok",
-            response: usertoken,
         });
+
+        const https = require('https')
+        var options = {
+            hostname: 'api-scanner.thaichana.com',
+            path: '/usertoken',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+            }
+        };
+
+        const req = https.request(options, ress => {
+            console.log(`statusCode: ${ress.statusCode}`)
+
+            ress.on('data', d => {
+                process.stdout.write(d)
+                return res.json({
+                    status: "ok",
+                    response: Buffer.from(d).toString(),
+                });
+            })
+        })
+
+        req.on('error', error => {
+            console.error(error)
+        })
+
+        req.write(data)
+        req.end()
+
+        // console.log(usertoken);
+
+        // return res.json({
+        //     status: "ok",
+        //     response: usertoken,
+        // });
     } catch (error) {
         console.log(error);
 
